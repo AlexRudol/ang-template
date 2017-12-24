8ba5812d5a010289ae4354c15535fb17bc47b8e0
Alex Rudol <alex.a.rudol@gmail.com>
2017-12-24 22:55:46 +1300

    Setting tabs and gaps.

--- config/helpers.js
+++ config/helpers.js
@@ -1,25 +1,22 @@
-/**
- * @author: tipe.io
- */
 const path = require('path');
 
 const EVENT = process.env.npm_lifecycle_event || '';
 
 /**
- * Helper functions.
+ * Helper functions
  */
 var ROOT = path.resolve(__dirname, '..');
 
 function hasProcessFlag(flag) {
-  return process.argv.join('').indexOf(flag) > -1;
+    return process.argv.join('').indexOf(flag) > -1;
 }
 
 function hasNpmFlag(flag) {
-  return EVENT.includes(flag);
+    return EVENT.includes(flag);
 }
 
 function isWebpackDevServer() {
-  return process.argv[1] && !! (/webpack-dev-server/.exec(process.argv[1]));
+    return process.argv[1] && !! (/webpack-dev-server/.exec(process.argv[1]));
 }
 
 
